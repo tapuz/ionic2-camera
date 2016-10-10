12d89538a0f8d9e8141a71711b3ea0c6cc694e52
Louis Van de Calseyde <vdc.louis@gmail.com>
2016-10-10 20:08:03 +0200

    init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public patient = {
+    base64Image: '',
+    id: null
+  };
+
+  submitted = false;
+
+  constructor(
+    public navCtrl: NavController
+  ) {}
+
+  takePicture () {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then(imageData => {
+      this.patient.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  onSubmit () {
+    if (this.patient.base64Image && this.patient.id) {
+      // do call
+      this.submitted = true;
+    }
+  }
+}
